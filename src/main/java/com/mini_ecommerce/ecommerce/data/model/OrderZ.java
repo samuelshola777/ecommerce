b68c74eba3d5738dbbcb9a3776030476093645dc
samuel shola <samuelshola14@gmail.com>
2023-04-13 15:39:00 +0100

    spring boot  E-commerce project, creating the entities

--- src/main/java/com/mini_ecommerce/ecommerce/data/model/OrderZ.java
+++ src/main/java/com/mini_ecommerce/ecommerce/data/model/OrderZ.java
@@ -1,7 +1,6 @@
 package com.mini_ecommerce.ecommerce.data.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
 import java.util.Set;
@@ -9,10 +8,11 @@ import java.util.Set;
 public class OrderZ {
     @OneToMany(cascade= CascadeType.ALL)
     private Set<Product> productList;
-    @OneToMany(cascade= CascadeType.ALL)
+    @OneToOne(cascade= CascadeType.ALL)
     private Customer customer;
     @OneToMany(cascade= CascadeType.ALL)
     private Address deliveryAddress;
+    @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
 }
